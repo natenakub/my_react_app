a3e4d26998176821c7916d356927c64641e2167e
natenakub <natenatthawut@gmail.com>
2023-06-19 16:17:11 +0700

    Prime_numbers

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import MyHeader from './MyHeader';
 import MyFooter from './MyFooter';
 
 function App() {
-  setInterval(UpdateTime, 1000);
-  let now = new Date().toLocaleTimeString('th-TH');
-  var [time, setTime] = useState(now);
+  const [time, setTime] = useState(new Date().toLocaleTimeString('th-TH'));
   const [num, setNum] = useState(0);
+  const [isPrimeNumber, setIsPrimeNumber] = useState(false);
 
-  function UpdateTime() {  
-    let newTime = new Date().toLocaleTimeString('th-TH');
-    setTime = newTime;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      updateTime();
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    const checkPrime = isPrime(num);
+    setIsPrimeNumber(checkPrime);
+  }, [num]);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString('th-TH');
+    setTime(newTime);
+  }
+
+  function isPrime(number) {
+    if (number < 2) {
+      return false;
+    }
+
+    for (let i = 2; i < number; i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
   return (
     <div>
       <MyHeader />
       <div className="t_name">
-        <div>ขนะนี้เวลา</div>
-        <div>{ time }</div>
+        <div>ขณะนี้เวลา</div>
+        <div>{time}</div>
         <h2>{num}</h2>
-        <button onClick={()=>setNum(num+1)}>Add</button>
-        <button onClick={()=>setNum(num-1)}>Delete</button>
+        <button onClick={() => setNum(num + 1)}>Add</button>
+        <button onClick={() => setNum(num - 1)}>Delete</button>
+        <div>{isPrimeNumber ? <p>เป็นจำนวนเฉพาะ</p> : <p>ไม่เป็นจำนวนเฉพาะ</p>}</div>
       </div>
       <MyFooter />
     </div>
